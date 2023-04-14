04fd2d08cfb3ecd1a4a959125ebbfdaa16e09761
tobit <franciscosavala01@gmail.com>
2023-04-13 21:15:23 -0300

    initial commit

---
+++ src/main/java/com/jpa/jpapracticerelations/JpaPracticeRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.jpapracticerelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPracticeRelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPracticeRelationsApplication.class, args);
+    }
+
+}
